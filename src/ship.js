7463b3095567a9aab5189a05e4b7bccd9831bad1
Maddie <115709272+Maddily@users.noreply.github.com>
2024-11-10 10:53:33 +0200

    Create Ship class

---
+++ src/ship.js
@@ -0,0 +1,25 @@
+export default class Ship {
+  #hits = 0;
+
+  constructor(length, name) {
+    this.shipLength = length;
+    this.name = name;
+  }
+
+  get hits() {
+    return this.#hits;
+  }
+
+  hit() {
+    if (this.#hits < this.shipLength) {
+      this.#hits += 1;
+    }
+  }
+
+  isSunk() {
+    if (this.#hits === this.shipLength) {
+      return true;
+    }
+    return false;
+  }
+}
